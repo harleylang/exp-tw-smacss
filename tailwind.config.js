4f3715bba13b4ed153251793550dae10f4d151a4
Harley Lang <harleylang+git@pm.me>
2023-12-09 17:29:03 +0000

    tailwind.config.js: add: colors

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./www/html/tailwind.html"],
+  theme: {
+    colors: {
+      primary: "#017edb",
+      "primary-dark": "#015b9e",
+      black: "#000",
+      grey: "#666",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
